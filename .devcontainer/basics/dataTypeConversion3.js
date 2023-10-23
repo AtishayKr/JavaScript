23c320a343dc90a438acd86b844853576feebfb2
Atishay Kumar <106793557+AtishayKr@users.noreply.github.com>
2023-10-23 16:20:19 +0000

    dataTypeConversion3

---
+++ .devcontainer/basics/dataTypeConversion3.js
@@ -0,0 +1,20 @@
+
+let score = "33asd";
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+
+// "33" => 33;
+// "331" => NaN (Not a Number);
+//"asd" => NaN
+// true => 1, false => 0;
+
+let scoreCard = true;
+let valueInString = String(scoreCard);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+//33 => "33";
+//true => "true";
+
