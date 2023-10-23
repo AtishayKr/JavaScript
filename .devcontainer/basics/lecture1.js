cb78cc0f46413d520d6c8a6b3eeaa61ee9d5e23e
Atishay Kumar <106793557+AtishayKr@users.noreply.github.com>
2023-10-23 14:18:43 +0000

    variables

---
+++ .devcontainer/basics/lecture1.js
@@ -0,0 +1,11 @@
+const userId = "Atishay123";
+let accountName = "Atishay";
+var branchName = "Khekra";
+
+//let is block scope and can be define only once
+
+//var is global scope and can be define as many times as you want
+
+//const once define not able to change
+
+// we should use let everywhere in new codes
