10668984343f996d9a416b56ef6c39ab8d000a27
Atishay Kumar <106793557+AtishayKr@users.noreply.github.com>
2023-10-23 16:29:43 +0000

    dataType-Summery

---
+++ .devcontainer/basics/dataType-Summery.js
@@ -0,0 +1,16 @@
+// premitive dataType
+// 7 type -> Number, BigInt, String, Boolen, null, undefine, Symbol, 
+let num = 123;
+let bigNumber = 1238493854789;
+let stringVal = "Atishay";
+let boolVal = true;
+let isOut = null; //null
+let val; // undefine
+let symbolVal = Symbol("123");  // Symbol(123)
+console.log(symbolVal);
+
+//refrenceType of nonPremitive
+// object, array, function => dataType will print object except function which will print finction
+
+let arr = [1, 2, 3];
+// console.log(typeof arr); // object
