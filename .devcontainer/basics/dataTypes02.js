471ea7f98ad7381cc50322cdcb391ac308b64aa8
Atishay Kumar <106793557+AtishayKr@users.noreply.github.com>
2023-10-23 14:24:50 +0000

    dataType

---
+++ .devcontainer/basics/dataTypes02.js
@@ -0,0 +1,18 @@
+let state;
+
+// premitive data type
+
+//number => 2 to power 53
+//bigint => for big integer
+//string => ""
+//boolean => true / false
+//null => standalone value
+//undefine => 
+//symbol => unique
+
+//Non premitive data type
+
+//object
+
+console.log(typeof undefined) // print undefine
+console.log(typeof null) // print object
