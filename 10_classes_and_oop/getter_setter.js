2491a2e4c5d4bf244f7ca75939afdc5e03e17bf1
Atishay Kumar <106793557+AtishayKr@users.noreply.github.com>
2023-10-27 19:30:18 +0000

    getter_setter completed

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -3,6 +3,19 @@ class User{
         this.email = email
         this.password = password
     }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
 }
 
 const obj = new User("atishay@gmail.com", "123");
